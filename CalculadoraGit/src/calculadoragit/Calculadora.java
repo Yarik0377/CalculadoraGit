a4a3595109f96e19e463a3d8d59c97b3effb2630
ALUMNO2425 <ALUMNO2425@DESKTOP-CDGKK4P>
2026-03-23 13:36:49 +0100

    Preparando release 1.0.0

--- CalculadoraGit/src/calculadoragit/Calculadora.java
+++ CalculadoraGit/src/calculadoragit/Calculadora.java
@@ -1,15 +1,20 @@
 package calculadoragit;
 
+// Version 1.0.0
 public class Calculadora {
+	//Metodo para sumar dos numeros
     public int sumar(int a, int b) {
         return a + b;
     }
+  //Metodo para restar dos numeros
     public int restar(int a, int b) {
         return a - b;
     }
+    //Metodo para multiplicar dos numeros
     public int multiplicar(int a, int b) {
         return a * b; 
     }
+  //Metodo para dividir dos numeros
     public double dividir(int a, int b) {
         return (double) a / b; 
     }
